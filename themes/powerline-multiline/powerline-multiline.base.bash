4b7a9c83dfddbe79a7d7ac44f2c5653c7390f720
Lawzy93 <32887870+Lawzy93@users.noreply.github.com>
2017-12-08 17:46:42 +1100

    test

--- themes/powerline-multiline/powerline-multiline.base.bash
+++ themes/powerline-multiline/powerline-multiline.base.bash
@@ -1,7 +1,7 @@
 . "$BASH_IT/themes/powerline/powerline.base.bash"
 
 function __powerline_last_status_prompt {
-  [[ "$1" -ne 0 ]] && echo "$(set_color ${LAST_STATUS_THEME_PROMPT_COLOR} -) ${1} ${normal}"
+  [[ "$1" -ne 0 ]] && echo "$(set_color ${TEXT_COLOR} -) ${1} ${normal}"
 }
 
 function __powerline_right_segment {
